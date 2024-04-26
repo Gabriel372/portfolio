13952f8821279a71354a1b284511988364383128
Gabriel372 <gabriel-dobt@hotmail.com>
2024-04-26 14:18:36 -0300

    update

--- src/pages/Header.tsx
+++ src/pages/Header.tsx
@@ -7,7 +7,7 @@ import { TstateTheme } from "@/types/Types";
 
 function Header() {
 const {ThemeIsDark} = useContext(MyContext) as TstateTheme ;
-const ThemeForComponent = ThemeIsDark ? 'text-white bg-gray-800 duration-500 border-b-gray-600 border border-t-0':'duration-500 bg-gradient-to-b from-gray-600 to-white'
+const ThemeForComponent = ThemeIsDark ? 'text-white bg-gray-1000 duration-700 border-b-gray-600 border border-t-0':'duration-800 bg-gradient-to-b from-gray-600 to-white'
 
 return (<header className={`${ThemeForComponent} dura`}>
 <nav className=" flex justify-around w-full h-16 items-center ">
